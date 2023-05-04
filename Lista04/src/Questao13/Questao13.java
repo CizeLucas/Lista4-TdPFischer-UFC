20614517c3566b5660b04acf0dae211e783b6a4c
CizeLucas <cizelucas@gmail.com>
2023-05-04 09:22:42 -0300

    update 7

--- Lista04/src/Questao13/Questao13.java
+++ Lista04/src/Questao13/Questao13.java
@@ -3,8 +3,33 @@ package Questao13;
 public class Questao13 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		ControleRemoto cr = new ControleRemoto(new TV());
+		
+		cr.turnOff(); //(A)
+		
+		cr.turnOn(); //(A)
+		
+		cr.status(); //(D)
+		
+		cr.volumeUp(); //(B)
+		cr.volumeUp(); //(B)
+		
+		cr.volumeDown(); //(B)
+		
+		cr.status(); //(D)
+		
+		cr.mute(); //(F)
+		
+		cr.goToChannel(20); //(E)
+		
+		cr.mute(); //(F)
+		
+		cr.channelDown(); //(C)
+		
+		cr.channelUp(); //(C)
+		
+		
 	}
 
 }
